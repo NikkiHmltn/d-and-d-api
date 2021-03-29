afd85c67b340e46bbc898a8683a1bb5aed52d729
nicole <nikkihmltn1@yahoo.com>
2021-03-29 11:07:59 -0700

    successfully connected to mongo atlas

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const url = process.env.MONGO_URI
+
+const connectionParams = {
+    useNewURLParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(url, connectionParams)
+    .then(() => {
+        console.log('Connected to Database')
+    })
+    .catch((err) => {
+        console.error('Error connecting to DB')
+    })
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
-    res.send('Hello hello hello')
+    res.status(200).json({msg: 'Connected'})
 })
 
 app.listen(3000, (req, res) => {
