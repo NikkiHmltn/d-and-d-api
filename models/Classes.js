e33fa9c07e2cfb961433fa31f91a54e9ed0e8444
nicole <nikkihmltn1@yahoo.com>
2021-03-30 18:54:16 -0700

    finish classes schema

--- models/Classes.js
+++ models/Classes.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose')
 
-const subClassSchema = new mongoose.Schema({
+const pathSchema = new mongoose.Schema({
     name: String,
     description: String,
+})
+
+const featuresSchema = new mongoose.Schema({
+    name: String,
+    description: String
+})
+
+const profienciesSchema = new mongoose.Schema({
+    armors: [String],
+    weapons: [String],
+    tools: [String],
+    saving_throws: [String],
+    skills: String
 })
 
 const classesSchema = new mongoose.Schema({
@@ -12,7 +25,15 @@ const classesSchema = new mongoose.Schema({
         unique: true,
     }, 
     description: String,
-    subclass: [subClassSchema],
+    hit_dice: String,
+    hit_points_at_first_level: String,
+    hit_points_at_higher_levels: String,
+    profiencies: [profienciesSchema],
+    spellcasting_ability: String,
+    features: [featuresSchema],
+    archetype: String,
+    paths: [pathSchema],
+
 })
 
 module.exports = mongoose.model("classes", classesSchema)
